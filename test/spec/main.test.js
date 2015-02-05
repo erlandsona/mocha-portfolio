ed47d531980efadbb3a9f210cb75b6f5d18aa57d
Austin Erlandson <austin@erlandson.com>
2015-02-05 11:33:17 -0600

    latest changes

--- test/spec/main.test.js
+++ test/spec/main.test.js
@@ -4,6 +4,28 @@
 describe('test suite', function () {
   it('should assert true', function () {
     true.should.be.true;
-    false.should.be.true;
+    false.should.be.false;
   });
 });
+
+
+describe('hello', function() {
+  it('should return world', function(){
+    hello().should.equal('world');
+  });
+});
+
+
+describe('getStock', function () {
+  it('should return a stock object', function (done) {
+    getStock('AAPL', function (stock) {
+      stock.Name.should.equal('Apple Inc');
+      done();
+    });
+
+//    getStock('MSFT', function (stock) {
+//      stock.Name.should.equal('Microsoft Crap');
+//    });
+  });
+});
+
